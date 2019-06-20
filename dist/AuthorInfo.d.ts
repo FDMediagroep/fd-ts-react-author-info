40a75bf886754c70aaba29135f666e6c0cd25513
Willem Liu <willem_liu@hotmail.com>
2019-06-20 15:14:46 +0200

    Using styled-components v5-beta

--- dist/AuthorInfo.d.ts
+++ dist/AuthorInfo.d.ts
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import { CardTypes } from "@fdmg/fd-card";
 export interface Props {
     cardStyle: CardTypes;
@@ -11,8 +11,5 @@ export interface Props {
     onLinkedInClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
     onTwitterClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
-export default class AuthorInfo extends PureComponent<Props, any> {
-    onFollowAuthorClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-    render(): JSX.Element;
-}
-export declare const AuthorInfoStyle: import("styled-components").GlobalStyleComponent<{}, import("styled-components").DefaultTheme>;
+export default function AuthorInfo(props: Props): JSX.Element;
+export declare const AuthorInfoStyle: import("styled-components").FlattenSimpleInterpolation;
--- src/AuthorInfo.tsx
+++ src/AuthorInfo.tsx
@@ -1,8 +1,8 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import Card, { getAllCardStyles, CardTypes } from "@fdmg/fd-card";
 import TypoGraphy, { getAllTextStyles } from "@fdmg/fd-typography";
 import {FollowButton, FollowButtonStyle, ButtonEditorial, ButtonEditorialStyles} from '@fdmg/fd-buttons';
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
 
 export interface Props {
     cardStyle: CardTypes;
@@ -16,42 +16,40 @@ export interface Props {
     onTwitterClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export default class AuthorInfo extends PureComponent<Props, any> {
-    onFollowAuthorClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if (this.props.onFollowAuthorClick) {
-            this.props.onFollowAuthorClick(e.currentTarget.getAttribute('data-selected') !== 'true');
+export default function AuthorInfo(props: Props) {
+    const onFollowAuthorClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        if (props.onFollowAuthorClick) {
+            props.onFollowAuthorClick(e.currentTarget.getAttribute('data-selected') !== 'true');
         }
-    }
+    };
 
-    render() {
-        return (
-            <>
-                <GlobalStyle/>
-                <Card cardStyle={this.props.cardStyle} className={`fd-author-info${this.props.className ? ` ${this.props.className}` : ''}`}>
-                    <div className="profile">
-                        <img src="https://images.fd.nl/ffa583e0c7fbc666934ecc326a88defc2773f4e9.jpeg?fit=crop&amp;crop=faces&amp;auto=format&amp;q=45&amp;cs=tinysrgb&amp;w=640&amp;h=640&amp;fm=jpg" alt="Nelleke Trappenburg"/>
-                        <TypoGraphy textStyle="card-h" className="fd-author-info-h"><h3>{this.props.title}</h3></TypoGraphy>
-                        <div className="description">{this.props.description}</div>
-                    </div>
-                    <div className="controls">
-                        <FollowButton
-                            selected={this.props.followed}
-                            buttonStyle={this.props.cardStyle === 'persoonlijk' ? 'persoonlijk' : 'default'}
-                            tag="Van onze redacteur"
-                            onClick={this.onFollowAuthorClick}
-                            followButtonText="Volg deze auteur"
-                        />
-                        {this.props.onEmailClick ? <ButtonEditorial onClick={this.props.onEmailClick}><i className="icon-envelope"/> E-mail</ButtonEditorial> : null}
-                        {this.props.onTwitterClick ? <ButtonEditorial onClick={this.props.onTwitterClick}><i className="icon-twitter"/>Twitter</ButtonEditorial> : null}
-                        {this.props.onLinkedInClick ? <ButtonEditorial onClick={this.props.onLinkedInClick}><i className="icon-linkedin-in"/>LinkedIn</ButtonEditorial> : null}
-                    </div>
-                </Card>
-            </>
-        );
-    }
+    return (
+        <>
+            <GlobalStyle/>
+            <Card cardStyle={props.cardStyle} className={`fd-author-info${props.className ? ` ${props.className}` : ''}`}>
+                <div className="profile">
+                    <img src="https://images.fd.nl/ffa583e0c7fbc666934ecc326a88defc2773f4e9.jpeg?fit=crop&amp;crop=faces&amp;auto=format&amp;q=45&amp;cs=tinysrgb&amp;w=640&amp;h=640&amp;fm=jpg" alt="Nelleke Trappenburg"/>
+                    <TypoGraphy textStyle="card-h" className="fd-author-info-h"><h3>{props.title}</h3></TypoGraphy>
+                    <div className="description">{props.description}</div>
+                </div>
+                <div className="controls">
+                    <FollowButton
+                        selected={props.followed}
+                        buttonStyle={props.cardStyle === 'persoonlijk' ? 'persoonlijk' : 'default'}
+                        tag="Van onze redacteur"
+                        onClick={onFollowAuthorClick}
+                        followButtonText="Volg deze auteur"
+                    />
+                    {props.onEmailClick ? <ButtonEditorial onClick={props.onEmailClick}><i className="icon-envelope"/> E-mail</ButtonEditorial> : null}
+                    {props.onTwitterClick ? <ButtonEditorial onClick={props.onTwitterClick}><i className="icon-twitter"/>Twitter</ButtonEditorial> : null}
+                    {props.onLinkedInClick ? <ButtonEditorial onClick={props.onLinkedInClick}><i className="icon-linkedin-in"/>LinkedIn</ButtonEditorial> : null}
+                </div>
+            </Card>
+        </>
+    );
 }
 
-const GlobalStyle = createGlobalStyle`
+const styles = css`
 .fd-author-info {
     .profile, .controls {
         padding: 12px 10px;
@@ -104,10 +102,12 @@ const GlobalStyle = createGlobalStyle`
 }
 `;
 
-export const AuthorInfoStyle = createGlobalStyle`
-${getAllCardStyles().globalStyle.rules}
-${getAllTextStyles(['card-h']).globalStyle.rules}
-${(FollowButtonStyle as any).globalStyle.rules}
-${(ButtonEditorialStyles as any).globalStyle.rules}
-${(GlobalStyle as any).globalStyle.rules}
+const GlobalStyle = createGlobalStyle`${styles}`;
+
+export const AuthorInfoStyle = css`
+${getAllCardStyles()}
+${getAllTextStyles(['card-h'])}
+${FollowButtonStyle}
+${ButtonEditorialStyles}
+${styles}
 `;
